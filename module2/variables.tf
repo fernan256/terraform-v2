9c23a3d24064128933af73db905d347f331cab6a
Diego Mayorga <diego.mayorga86@gmail.com>
2020-04-16 21:00:37 -0300

    Add initial files for mod2

---
+++ module2/variables.tf
@@ -0,0 +1,9 @@
+# VARIABLES
+##################################################################################
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "subnet_count"   {
+  default = 2
+}
